3e2c7cf14ea25fcd1594fd43dbc9e2a247894256
Đỗ Trung Đức <dotrungduc1908@gmail.com>
2024-09-26 12:57:49 +0700

    changes in design

--- src/main/java/com/example/oopproj/HelloApplication.java
+++ src/main/java/com/example/oopproj/HelloApplication.java
@@ -10,14 +10,15 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-//        stage.setTitle("pp");
-//        stage.setScene(scene);
-//        stage.show();
+       FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Dictionary.fxml"));
+       Scene scene = new Scene(fxmlLoader.load(),1530,800);
+        stage.setTitle("pp");
+       stage.setScene(scene);
+       stage.setFullScreen(true);
+       stage.show();
     }
 
     public static void main(String[] args) {
-        //launch();
+        launch();
     }
 }
